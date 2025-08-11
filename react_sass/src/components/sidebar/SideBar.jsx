10ba6a07830ebdcffa5b0b775c5b3f3c7a42ae65
itsmsarah <sarahmendesandrade1@gmail.com>
2025-08-11 17:08:01 -0300

    style:Start to style the sydebar and i added a photo of myself

--- react_sass/src/components/sidebar/SideBar.jsx
+++ react_sass/src/components/sidebar/SideBar.jsx
@@ -1,6 +1,19 @@
+import '../sidebar/sidebar.sass'
+
+import MinhaImg from '../../assets/eu-img.jpg.jpg'
+
 const SideBar =()=>{
     //letra maiuscula pq indica que estamos criando um componente 
-    return <div>SideBar</div>
+    return (
+          <aside id="sidebar">
+            <p>imagem</p>
+            <p className="title"></p>
+            <p>redes sociais</p>
+            <p>informações de contato</p>
+            <a href="" className="btn">Download Currículo</a>
+    </aside>
+    )
+  
 }
 
 export default SideBar
